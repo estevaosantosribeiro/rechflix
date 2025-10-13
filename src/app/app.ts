dc1d4966c669be25ccf526308fe26ad632834e19
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-10-13 19:52:04 -0300

    :sparkles: feat: implementação da listagem de mídias com dados da api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { Header } from "./components/header/header";
 import { CommonModule } from '@angular/common';
 import { Sidebar } from "./components/sidebar/sidebar";
+import { TmdbService } from './services/tmdb-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, CommonModule, Sidebar],
+  imports: [Header, CommonModule, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('rechflix');
+  protected readonly tmdbService = inject(TmdbService);
+
+  protected readonly midias = this.tmdbService.selecionarFilmes();
 }
